a7f7ed6224a5703a072dc1716b7ddb38411f05d5
juanfegale <jgaleanoro@unal.edu.co>
2025-08-17 02:10:39 -0500

    Actualizar PlayerMove.cs

--- PlanetProto/Assets/Scripts/PlayerMove.cs
+++ PlanetProto/Assets/Scripts/PlayerMove.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    [export] private GameObject modelo;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
